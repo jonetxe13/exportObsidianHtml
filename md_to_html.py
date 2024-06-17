32b887875c97d76a165417d142603104e332cbc2
jonetxe13 <jonetxe.sasia13@gmail.com>
2024-06-17 22:38:49 +0200

    jflajk

--- md_to_html.py
+++ md_to_html.py
@@ -2,11 +2,10 @@ import os
 import sys
 import re
 import subprocess
-from urllib.parse import urlparse
 
 def convert_md_to_html(md_file_path, html_dir):
+    print("la terminacion es: " + md_file_path[-7:])
     if md_file_path[-7:] == '.png.md' or md_file_path[-7:] == '.jpg.md' or md_file_path[-7:] == '.pdf.md':
-        print(['cp', os.path.join("/home/jonetxe13/Desktop/obsidian/Universidad/Imagenes/ANALISIS MATEMATICO/", os.path.basename(md_file_path[:-3])), os.path.join(html_dir, os.path.basename(md_file_path[:-3]))])
         subprocess.run(['cp', os.path.join("/home/jonetxe13/Desktop/obsidian/", os.path.basename(md_file_path[:-3])), os.path.join(html_dir, os.path.basename(md_file_path[:-3]))])
         return
     if not os.path.isfile(md_file_path):
@@ -14,7 +13,9 @@ def convert_md_to_html(md_file_path, html_dir):
     html_file_path = os.path.join(html_dir, os.path.splitext(os.path.basename(md_file_path))[0] + '.html')
     with open(md_file_path, 'r') as file:
         content = file.read()
-        content = re.sub(r'\[\[(.*?)\]\]', lambda m: '<a href="{}.html">{}</a>'.format(m.group(1), m.group(1)) if m.group(1)[-3:] == '.md' else '<img src="{}" style="max-width:550px;">'.format(m.group(1), m.group(1)), content)
+        print(md_file_path)
+        print("el file path termina en .jpg.md ", md_file_path[-7:] == '.jpg.md')
+        content = re.sub(r'\[\[(.*?)\]\]', lambda m: '<img src="{}" style="max-width:550px;">'.format(m.group(1), m.group(1)) if (m.group(1)[-7:] != '.jpg.md' or m.group(1)[-7:] != '.png.md') else '<img src="{}" style="max-width:550px;">'.format(m.group(1)), content)
     with open(md_file_path + ".tmp", 'w') as file:
         file.write(content)
     subprocess.run(['pandoc','--template=simple.latex', md_file_path + ".tmp", '-o', html_file_path])
@@ -37,7 +38,7 @@ def process_file(md_file_path, html_dir):
     with open(md_file_path, 'r') as file:
         content = file.read()
     md_links = re.findall(r'\[\[(.*)\]\]', content)
-    md_file_paths = [os.path.join("/home/jonetxe13/Desktop/obsidian/", link + '.md') for link in md_links]
+    md_file_paths = [os.path.join("/home/jonetxe13/Desktop/obsidian/Universidad/Asignaturas/1/", link + '.md') for link in md_links]
     convert_md_to_html(md_file_path, html_dir)
     for linked_md_file_path in md_file_paths:
         # print(linked_md_file_path)
@@ -45,7 +46,7 @@ def process_file(md_file_path, html_dir):
 
 def main():
 
-    html_dir = os.path.expanduser('~/Desktop/html_dir')
+    html_dir = os.path.expanduser('~/Desktop/html_dir/Calculo')
     if len(sys.argv) > 1:
         md_file_path = sys.argv[1]
         html_dir = os.path.join(html_dir, os.path.splitext(os.path.basename(md_file_path))[0])
